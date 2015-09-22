7ecbd15eb3835d6ee3ec1658c65e1c3de744cd5f
Tom Barton <grez.cz@gmail.com>
2015-09-22 19:15:15 +0200

    Added .neon boolean checker

---
+++ src/CodeCheckers/NeonChecker.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CodeCheckers;
+
+use Nette\Object;
+use Nette\Utils\Strings;
+
+
+
+class NeonChecker extends Object
+{
+
+	public static function createBooleanValuesChecker()
+	{
+		return function ($checker, $s) {
+			if (!$checker->is('neon')) {
+				return;
+			}
+
+			$lines = explode("\n", $s);
+			foreach ($lines as &$line) {
+				if (preg_match('~^(.*):( )?(yes|on|no|off)$~i', $line)) {
+					$message = sprintf('Boolean values should be true/false: %s', $line);
+					$checker->fix($message);
+					$line = preg_replace('~:( )?(yes|on)$~i', ': true', $line);
+					$line = preg_replace('~:( )?(no|off)$~i', ': false', $line);
+				}
+			}
+			return implode("\n", $lines);
+		};
+	}
+}
